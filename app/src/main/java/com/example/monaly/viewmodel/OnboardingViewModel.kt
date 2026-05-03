2fafc703a3b926bf9453b1c3c842f62b0e5531db
officialdelffin <workdelffin@gmail.com>
2026-05-03 15:57:11 -0300

    commit 14

---
+++ app/src/main/java/com/example/monaly/viewmodel/OnboardingViewModel.kt
@@ -0,0 +1,49 @@
+
+// Pacotes :
+package com.example.monaly.viewmodel
+
+
+// Importações :
+import com.example.monaly.R
+import com.example.monaly.model.OnboadingPage
+
+
+// Essa classe é responsável por generenciar as informações das telas de onboarding, como foto, titulo e descrição :
+class OnboardingViewModel {
+
+
+    // Lista com os dados da apresentação :
+    val OnboardingInformation = listOf(
+
+
+        OnboadingPage(
+
+            title = R.string.onboarding_label_welcome,
+            description = R.string.onboarding_description_one
+
+        ),
+
+        OnboadingPage(
+
+
+            title = R.string.onboarding_memories,
+            description = R.string.ondoarding_description_two
+
+
+        ),
+
+        OnboadingPage(
+
+
+
+            title = R.string.onboarding_history,
+            description = R.string.onboarding_description_three
+
+
+        )
+
+
+    )
+
+
+}

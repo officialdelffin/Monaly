49d150b70dc378dcb605cd463d12bc700bda659e
officialdelffin <workdelffin@gmail.com>
2026-06-02 21:22:43 -0300

    commit 22

--- app/src/main/java/com/example/monaly/ui/fragment/onboarding/OnboardingThree.kt
+++ app/src/main/java/com/example/monaly/ui/fragment/onboarding/OnboardingThree.kt
@@ -11,16 +11,26 @@ import com.example.monaly.R
 class OnboardingThree : Fragment() {
 
 
+    // Chamando o onCreateView para construir a interface da tela :
     override fun onCreateView(
 
 
-        inflater: LayoutInflater, container: ViewGroup?,
+        // Convertendo o XML em View (Elementos graficos) :
+        inflater: LayoutInflater,
+
+
+        // Define onde o fragment sera usado :
+        container: ViewGroup?,
+
+
+        // Objeto que salva os dados caso a tela seja recriada, como mudar a orientação da tela:
         savedInstanceState: Bundle?
 
 
     ): View? {
 
 
+        // Inflando e retornando o XML em forma de view do fragment onboarding one :
         return inflater.inflate(R.layout.fragment_onboarding_three, container, false)
 
 

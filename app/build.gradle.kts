6abee818142272bf61619c692066a3c5687a229e
officialdelffin <workdelffin@gmail.com>
2026-07-20 11:00:27 -0300

    commit 64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("io.coil-kt:coil:2.6.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/monaly/ui/fragment/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/monaly/ui/fragment/onboarding/OnboardingFragment.kt
@@ -1,113 +1,121 @@
-    package com.example.monaly.ui.fragment.onboarding
-
-    import android.os.Bundle
-    import androidx.fragment.app.Fragment
-    import android.view.LayoutInflater
-    import android.view.View
-    import android.view.ViewGroup
-    import android.widget.ImageView
-    import android.widget.TextView
-    import com.example.monaly.R
-    import com.example.monaly.model.OnboadingPage
+package com.example.monaly.ui.fragment.onboarding
 
 
-    class OnboardingFragment : Fragment() {
+// Importações :
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import coil.load
+import com.example.monaly.R
+import com.example.monaly.model.OnboadingPage
 
 
-        // Chamando o onCreateView para construir a interface da tela :
-        override fun onCreateView(
+class OnboardingFragment : Fragment() {
 
 
-            // Convertendo o XML em View (Elementos graficos) :
-            inflater: LayoutInflater,
+    // Chamando o onCreateView para construir a interface da tela :
+    override fun onCreateView(
 
 
-            // Define onde o fragment sera usado :
-            container: ViewGroup?,
+        // Convertendo o XML em View (Elementos graficos) :
+        inflater: LayoutInflater,
 
 
-            // Objeto que salva os dados caso a tela seja recriada, como mudar a orientação da tela:
-            savedInstanceState: Bundle?
+        // Define onde o fragment sera usado :
+        container: ViewGroup?,
 
 
-        ): View? {
+        // Objeto que salva os dados caso a tela seja recriada, como mudar a orientação da tela :
+        savedInstanceState: Bundle?
 
 
-            // Inflando o layout XML e guardamos dentro da variável view :
-            val view = inflater.inflate(R.layout.fragment_onboarding, container, false)
+    ): View? {
 
 
-            // Atributos :
-
-            // Pegando e vinculando os componentes que o fragment precisa :
-            val title : TextView = view.findViewById(R.id.textTitle)
-            val description : TextView = view.findViewById(R.id.textDescription)
-            val image : ImageView = view.findViewById(R.id.imageBackground)
+        // Inflando o layout XML e guardamos dentro da variável view :
+        val view = inflater.inflate(R.layout.fragment_onboarding, container, false)
 
 
-            // Aqui é onde verificamos se tem dados enviados para a tela do fragment, se estiver como titulo e descrição vamos fazer a substituição para que não trave o app:
-            arguments?.let {
+        // Atributos :
 
 
-                // Faz a busca e a troca com base na chave que definimos :
-                val titleResId = it.getInt(KEY_TITLE_ONBOARDING)
-                val descriptionResId = it.getInt(KEY_DESCRIPTION_ONBOARDING)
-                title.setText(titleResId)
-                description.setText(descriptionResId)
-                image.setImageResource(it.getInt(KEY_IMAGE_ONBOARDING))
+        // Pegando e vinculando os componentes que o fragment precisa :
+        val title : TextView = view.findViewById(R.id.textTitle)
+        val description : TextView = view.findViewById(R.id.textDescription)
+        val image : ImageView = view.findViewById(R.id.imageBackground)
 
 
-            }
+        // Aqui é onde verificamos se tem dados enviados para a tela do fragment :
+        arguments?.let {
 
 
-            // Retornando a variavel view que faz com que o XML em forma de view do fragment onboarding one :
-            return view
+            // Faz a busca e a troca com base na chave que definimos :
+            val titleResId = it.getInt(KEY_TITLE_ONBOARDING)
+            val descriptionResId = it.getInt(KEY_DESCRIPTION_ONBOARDING)
+            title.setText(titleResId)
+            description.setText(descriptionResId)
+
+
+            // Substituindo o método nativo pelo Coil para carregar a imagem de forma assíncrona :
+            image.load(it.getInt(KEY_IMAGE_ONBOARDING))
 
 
         }
 
 
-        // Definições de chaves:
-
-        // Criando as chaves de acesso que trafegam no aplicativo
-        companion object {
+        // Retornando a variavel view que faz com que o XML em forma de view do fragment onboarding one :
+        return view
 
 
-            // Criando as chaves :
-            const val KEY_TITLE_ONBOARDING = "title"
-            const val KEY_DESCRIPTION_ONBOARDING = "description"
-            const val KEY_IMAGE_ONBOARDING = "image"
+    }
 
 
-            // Função responsável por criar os fragments e preencher os dados vinculado aos elementos do fragment
-            fun createNewFragment (dataPageP : OnboadingPage) : OnboardingFragment{
+    // Definições de chaves :
 
 
-                // Pegando os dados da OnboardingPage para criar o fragment :
-                val dataPage = Bundle().apply {
+    // Criando as chaves de acesso que trafegam no aplicativo :
+    companion object {
 
 
-                    putInt(KEY_TITLE_ONBOARDING, dataPageP.title)
-                    putInt(KEY_DESCRIPTION_ONBOARDING, dataPageP.description)
-                    putInt(KEY_IMAGE_ONBOARDING, dataPageP.image)
+        // Criando as chaves :
+        const val KEY_TITLE_ONBOARDING = "title"
+        const val KEY_DESCRIPTION_ONBOARDING = "description"
+        const val KEY_IMAGE_ONBOARDING = "image"
 
 
-                }
+        // Função responsável por criar os fragments e preencher os dados vinculado aos elementos do fragment :
+        fun createNewFragment (dataPageP : OnboadingPage) : OnboardingFragment{
 
 
-                val fragment = OnboardingFragment()
+            // Pegando os dados da OnboardingPage para criar o fragment :
+            val dataPage = Bundle().apply {
 
 
-                fragment.arguments = dataPage
-
-
-                return fragment
+                putInt(KEY_TITLE_ONBOARDING, dataPageP.title)
+                putInt(KEY_DESCRIPTION_ONBOARDING, dataPageP.description)
+                putInt(KEY_IMAGE_ONBOARDING, dataPageP.image)
 
 
             }
+
+
+            val fragment = OnboardingFragment()
+
+
+            fragment.arguments = dataPage
+
+
+            return fragment
 
 
         }
 
 
     }
+
+
+}

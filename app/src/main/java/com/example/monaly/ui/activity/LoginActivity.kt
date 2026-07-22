6bf9bef43cf1df074993de3668a66ef8af164315
officialdelffin <workdelffin@gmail.com>
2026-07-22 17:04:35 -0300

    commit 81

--- app/src/main/java/com/example/monaly/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/monaly/ui/activity/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.monaly.ui.activity
 
+
+// Importações :
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,15 +9,31 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.monaly.R
 
+
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) {
+
+
+            v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
+
     }
+
+
 }

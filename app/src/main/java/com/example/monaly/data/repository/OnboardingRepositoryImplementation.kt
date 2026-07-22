0dec941117379cb8fe5b469c6f439c01d8b18671
officialdelffin <workdelffin@gmail.com>
2026-07-22 13:32:05 -0300

    commit 74

---
+++ app/src/main/java/com/example/monaly/data/repository/OnboardingRepositoryImplementation.kt
@@ -0,0 +1,9 @@
+package com.example.monaly.data.repository
+
+class OnboardingRepositoryImplementation {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/monaly/domain/repositoy/OnboardingRepository.kt
@@ -0,0 +1,15 @@
+package com.example.monaly.domain.repositoy
+
+
+// Importações :
+import com.example.monaly.domain.model.OnboadingPage
+
+
+// Interface responsável por criar o contrato de uma devolução de uma lista :
+interface OnboardingRepository {
+
+
+
+
+
+}

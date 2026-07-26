e0bffb1019594f0dee234a91370f1bc526b7d2d3
officialdelffin <workdelffin@gmail.com>
2026-07-26 13:08:26 -0300

    99

---
+++ app/src/main/java/com/example/monaly/core/network/ConnectivityNetworkMonitor.kt
@@ -0,0 +1,14 @@
+package com.example.monaly.core.network
+
+
+// Importações :
+
+
+// Classe que implementa e faz o monitoramento em tempo real da placa de rede do dispositivo :
+class ConnectivityNetworkMonitor {
+
+
+
+
+
+}

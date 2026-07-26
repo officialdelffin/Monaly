50ad9ee2e3f663bc34649032fbece5f2b9624aab
officialdelffin <workdelffin@gmail.com>
2026-07-26 13:22:18 -0300

    100

--- app/src/main/java/com/example/monaly/core/network/ConnectivityNetworkMonitor.kt
+++ app/src/main/java/com/example/monaly/core/network/ConnectivityNetworkMonitor.kt
@@ -2,13 +2,22 @@ package com.example.monaly.core.network
 
 
 // Importações :
+import android.content.Context
+import android.net.ConnectivityManager
+import kotlinx.coroutines.flow.Flow
 
 
 // Classe que implementa e faz o monitoramento em tempo real da placa de rede do dispositivo :
-class ConnectivityNetworkMonitor {
+class ConnectivityNetworkMonitor (private val context : Context) : NetworkMonitor{
 
 
+    // Pegando a ferramenta de rede do android usando o passaporte context :
+    private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
+
+    // Sobrescrevendo a propriedadade que o NetworkMonitor exige a criação :
+    override val isOnline: Flow<Boolean>
+        get() = TODO("Not yet implemented")
 
 
 }

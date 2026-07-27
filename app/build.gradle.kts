f31f8140624707add1a97540d3f71ad928aa5cf4
officialdelffin <workdelffin@gmail.com>
2026-07-27 11:55:40 -0300

    commit 105

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("io.coil-kt:coil:2.6.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/monaly/ui/viewmodel/OnboardingViewModel.kt
+++ app/src/main/java/com/example/monaly/ui/viewmodel/OnboardingViewModel.kt
@@ -6,7 +6,10 @@ import com.example.monaly.domain.model.OnboadingPage
 import com.example.monaly.data.repository.OnboardingRepositoryImplementation
 import com.example.monaly.domain.repository.OnboardingRepository
 import androidx.lifecycle.ViewModel
-import com.example.monaly.core.network.ConnectivityNetworkMonitor
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import com.example.monaly.core.network.NetworkMonitor
 
 // O ViewModel recebe o repositório como uma dependência para funcionar :
@@ -25,6 +28,28 @@ class OnboardingViewModel(
 
     // A variável agora chama a função do repositório em vez de fabricar a lista :
     val OnboardingInformation: List<OnboadingPage> = repository.getPages()
+
+
+    // Criando a variável de estado que a Activity vai observar depois :
+    val isOnline: StateFlow<Boolean> = networkMonitor.isOnline
+
+
+        .stateIn(
+
+
+            // O escopo seguro do ViewModel que garante que o fluxo pare se o app fechar :
+            scope = viewModelScope,
+
+
+            // Regra de compartilhamento (mantém ativo por 5 segundos após a tela sumir para evitar reinícios desnecessários) :
+            started = SharingStarted.WhileSubscribed(5000),
+
+
+            // Valor inicial antes do monitor terminar a primeira checagem :
+            initialValue = false
+
+
+        )
 
 
 }

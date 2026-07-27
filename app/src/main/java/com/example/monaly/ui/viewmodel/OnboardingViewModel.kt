c8e862ff7119aaaa947f065f73fea75c536012e4
officialdelffin <workdelffin@gmail.com>
2026-07-27 11:38:33 -0300

    commit 96

--- app/src/main/java/com/example/monaly/ui/viewmodel/OnboardingViewModel.kt
+++ app/src/main/java/com/example/monaly/ui/viewmodel/OnboardingViewModel.kt
@@ -5,17 +5,20 @@ package com.example.monaly.ui.viewmodel
 import com.example.monaly.domain.model.OnboadingPage
 import com.example.monaly.data.repository.OnboardingRepositoryImplementation
 import com.example.monaly.domain.repository.OnboardingRepository
-
+import androidx.lifecycle.ViewModel
+import com.example.monaly.core.network.ConnectivityNetworkMonitor
+import com.example.monaly.core.network.NetworkMonitor
 
 // O ViewModel recebe o repositório como uma dependência para funcionar :
 class OnboardingViewModel(
 
 
     // Instanciando temporariamente a implementação manual (pagaremos essa dívida técnica com Injeção de Dependência depois) :
-    private val repository: OnboardingRepository = OnboardingRepositoryImplementation()
+    private val repository: OnboardingRepository = OnboardingRepositoryImplementation(),
+    private val networkMonitor: NetworkMonitor
 
 
-) {
+) : ViewModel() {
 
 
     // A variável agora chama a função do repositório em vez de fabricar a lista :

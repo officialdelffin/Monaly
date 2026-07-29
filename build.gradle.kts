c67a14fea6827e311c284983c655f26ce6d28d4a
officialdelffin <workdelffin@gmail.com>
2026-07-29 11:00:39 -0300

    commit 107

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
+
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    // Plugin de serviços do Google - exigencia do Firebase :
+    id("com.google.gms.google-services") version "4.5.0" apply false
+
+
 }

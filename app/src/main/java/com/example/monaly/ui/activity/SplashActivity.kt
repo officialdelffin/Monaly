59937297d233a58a0bd97c3b72966517ea541671
officialdelffin <workdelffin@gmail.com>
2026-08-04 15:31:29 -0300

    commit 134

--- app/src/main/java/com/example/monaly/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/monaly/ui/activity/SplashActivity.kt
@@ -1,21 +1,53 @@
 package com.example.monaly.ui.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.monaly.R
 
+// Importações :
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.monaly.ui.activity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+// Tela inicial que atua fazendo a conferencia se há login feito ou não :
 class SplashActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        // O Firebase gerencia os tokens de sessão automaticamente e o currentUser verifica se há um token válido salvo e criptografado no celular :
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+
+        // Se não for nulo, tem usuário, roteamos direto para a MainActivity pulando Onboarding e Login :
+        if (currentUser != null) {
+
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+
         }
+
+
+        // Se for nulo, ninguém logado, mandamos para o fluxo inicial do Onboarding :
+        else {
+
+
+            startActivity(Intent(this, OnboardingActivity::class.java))
+
+
+        }
+
+
+        // Encerramos a Splash para destruí-la da memória assim impedindo que o usuário volte para essa tela preta se apertar o botão 'Voltar' do celular :
+        finish()
+
+
     }
+
+
 }

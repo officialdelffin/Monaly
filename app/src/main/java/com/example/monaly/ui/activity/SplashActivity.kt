2afbc846dff37d69c1585d73436c88aa51f7b428
officialdelffin <workdelffin@gmail.com>
2026-08-04 15:27:16 -0300

    commit 133

--- app/src/main/java/com/example/monaly/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/monaly/ui/activity/SplashActivity.kt
@@ -1,7 +1,5 @@
 package com.example.monaly.ui.activity
 
-
-// Importações :
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,31 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.monaly.R
 
-
-// Classe principal de execução :
-class MainActivity : AppCompatActivity() {
-
-
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-
-
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
-
-
     }
-
-
 }

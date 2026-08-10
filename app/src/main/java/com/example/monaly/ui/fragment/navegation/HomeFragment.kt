62b9b1ce2676e884eff3f116276fb62f74fbe7bb
officialdelffin <workdelffin@gmail.com>
2026-08-10 16:56:39 -0300

    commit 169

--- app/src/main/java/com/example/monaly/ui/fragment/navegation/HomeFragment.kt
+++ app/src/main/java/com/example/monaly/ui/fragment/navegation/HomeFragment.kt
@@ -1,9 +1,36 @@
 package com.example.monaly.ui.fragment.navegation
 
 
+// Importações :
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.monaly.R
+import com.example.monaly.ui.adapter.HomePagerAdapter
 
 
-// Usamos o construtor direto do Fragment para injetar o layout sem precisar de boilerplate :
-class HomeFragment : Fragment(R.layout.fragment_home)
+// Usando o construtor direto do Fragment para injetar o layout sem precisar de boilerplate :
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+    // Usando o onViewCreated para ter segurança de que o layout XML já foi totalmente carregado :
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // Encontramos o componente de arrastar na tela :
+        val viewPager = view.findViewById<ViewPager2>(R.id.viewPagerHome)
+
+
+        // Instanciamos o adaptador e conectamos ao ViewPager2 :
+        val adapter = HomePagerAdapter(this)
+        viewPager.adapter = adapter
+
+
+    }
+
+
+}

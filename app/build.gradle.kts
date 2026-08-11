fdae01300bec5d6ebe0357b2530b89bad50f4d6e
officialdelffin <workdelffin@gmail.com>
2026-08-11 11:34:27 -0300

    commit 175

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,6 @@ kotlin {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
     implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
     implementation("androidx.credentials:credentials:1.2.2")

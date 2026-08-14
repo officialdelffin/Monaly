d32db5732e0d819d01cffabf9c643a1074679847
officialdelffin <workdelffin@gmail.com>
2026-08-14 10:14:50 -0300

    commit 214

--- app/src/main/java/com/example/monaly/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/monaly/ui/activity/RegisterActivity.kt
@@ -1,5 +1,7 @@
 package com.example.monaly.ui.activity
 
+
+// Importações :
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,15 +9,32 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.monaly.R
 
+
+// Tela responsável por coletar os dados do novo usuário :
 class RegisterActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+
+        // Ajuste das barras de sistema :
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
+
     }
+
+
 }

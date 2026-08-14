ecdc35647e80f2ead39f952e36d91e4941e291ba
officialdelffin <workdelffin@gmail.com>
2026-08-14 14:54:00 -0300

    commit 220

---
+++ app/src/main/java/com/example/monaly/ui/fragment/useregister/RegisterFragment.kt
@@ -0,0 +1,26 @@
+package com.example.monaly.ui.fragment.useregister
+
+// Importações :
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.monaly.R
+
+
+// Fragment responsável por capturar o e-mail na primeira etapa do cadastro :
+class RegisterEmailFragment : Fragment(R.layout.fragment_register) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // Aqui faremos a captura do texto digitado no próximo passo da lógica :
+
+
+    }
+
+
+}

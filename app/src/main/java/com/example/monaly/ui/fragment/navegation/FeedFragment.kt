841b919b430cb1fd8950b3ce46091ca00bfc5f85
officialdelffin <workdelffin@gmail.com>
2026-08-27 17:29:02 -0300

    commit 275

--- app/src/main/java/com/example/monaly/ui/fragment/navegation/FeedFragment.kt
+++ app/src/main/java/com/example/monaly/ui/fragment/navegation/FeedFragment.kt
@@ -4,7 +4,6 @@ package com.example.monaly.ui.fragment.navegation
 // Importações necessárias :
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest

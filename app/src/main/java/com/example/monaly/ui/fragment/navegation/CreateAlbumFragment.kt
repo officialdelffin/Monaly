4fb00d7b9a6bcd69d535a92d6c410f19e63eeb35
officialdelffin <workdelffin@gmail.com>
2026-08-31 11:47:32 -0300

    commit 296

--- app/src/main/java/com/example/monaly/ui/fragment/navegation/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/monaly/ui/fragment/navegation/CreateAlbumFragment.kt
@@ -48,8 +48,12 @@ class CreateAlbumFragment : Fragment() {
 
 
     override fun onCreateView(
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
 
 
